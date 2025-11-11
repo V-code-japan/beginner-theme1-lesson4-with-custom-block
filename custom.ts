3c3811b35fdb32b5955248276c94279062c001c5
V-code-japan <minecraft@e-sia.jp>
2025-11-11 17:23:15 +0900

    カスタムブロックの作成

--- custom.ts
+++ custom.ts
@@ -4,35 +4,51 @@
 * 詳しくはこちらを参照してください：https://minecraft.makecode.com/blocks/custom
 */
 
-// enum MyEnum {
-//     //% block="one"
-//     One,
-//     //% block="two"
-//     Two
-// }
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two,
+
+    Three
+}
 
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon=""
-// namespace custom {
-//     /**
-//      * TODO: describe your function here
-//      * @param n describe parameter here, eg: 5
-//      * @param s describe parameter here, eg: "Hello"
-//      * @param e describe parameter here
-//      */
-//     //% block
-//     export function foo(n: number, s: string, e: MyEnum): void {
-//         // Add code here
-//     }
+//% weight=100 color=#ff4500 icon=""
+namespace custom {
+    /**
+     * TODO: エージェントがブロックを1つ置いて左に移動する
+     * @param block describe block name, eg: GRASS
+     */
+    //% block
+    export function ブロックを1つおく(block: string): void {
+        agent.setItem(block, 1, 1)
+        agent.place(FORWARD)
+        agent.move(LEFT, 1)
+    }
 
-//     /**
-//      * TODO: describe your function here
-//      * @param value describe value here, eg: 5
-//      */
-//     //% block
-//     export function fib(value: number): number {
-//         return value <= 1 ? value : fib(value -1) + fib(value - 2);
-//     }
-// }
+    /**
+     * TODO: エージェントがブロックを指定した長さだけ左に並べていく
+     * @param block describe block name, eg: GRASS
+     * @param length describe how many blocks agent sets, eg: 5
+     */
+    //% block
+    export function ブロックを1列ならべる(block: string, length: number): void {
+        for (let index = 0; index < length - 1; index++) {
+            agent.setItem(block, 1, 1)
+            agent.place(FORWARD)
+            agent.move(LEFT, 1)
+        }
+    }
+
+    /**
+     * TODO: エージェントが次の位置へ移動する
+     * @param length number of blocks agent moves to right, eg: 5
+     */
+    export function 次の位置に移動する(length: number): void {
+        agent.move(UP, 1)
+        agent.move(RIGHT, length)
+    }
+}
